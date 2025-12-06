ada34c03888a5c9a8eb6ff237cfc57eb9fdecc22
DilaraButGold <dilaraaltinisik784@gmail.com>
2025-12-07 01:03:47 +0300

    chore: Prepare for deployment (dynamic port & build scripts)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,28 @@
 import express, { Request, Response } from 'express';
 import accountRoutes from './routes/account.routes';
 import transactionRoutes from './routes/transaction.routes';
-import authRoutes from './routes/auth.routes'; // 1. Yeni Rota
+import authRoutes from './routes/auth.routes';
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/auth', authRoutes); // 2. Bağla
+// Rotalar
+app.use('/auth', authRoutes);
 app.use('/accounts', accountRoutes);
 app.use('/transactions', transactionRoutes);
 
+// Sağlık Kontrolü (Render bunu sık sık kontrol eder)
 app.get('/', (req: Request, res: Response) => {
-    res.json({ message: "SecureWallet API Çalışıyor! 💸", status: "active" });
+    res.json({
+        message: "SecureWallet API Canlıda! 🚀",
+        status: "active",
+        timestamp: new Date()
+    });
 });
 
 app.listen(port, () => {
-    console.log(`🏦 Banka şubesi http://localhost:${port} adresinde açıldı!`);
+    console.log(`🏦 Banka şubesi port ${port} üzerinde açıldı!`);
 });
